1b91584cba640bd47d04ccd0c8dcfb8b4d87ab7b
wendongsheng <18637117435@163.com>
2020-11-24 17:11:02 +0800

    first commit

---
+++ upload.sh
@@ -0,0 +1,1 @@
+pod repo push SJModularizationPod SJAccountModule.podspec --verbose --allow-warnings --use-libraries --sources='https://github.com/ScienJoyShop/SJModularizationPod.git,https://github.com/CocoaPods/Specs.git'
